82b650b9e4b3992b41aafa5de9dddab295e26b31
Admiral-Simo <admiralsimo16@gmail.com>
2024-08-06 01:18:45 +0100

    feat: upload file to server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"passportScanner/views/pages"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +12,16 @@ func main() {
 
 	r.GET("/", func(c *gin.Context) {
 		pages.Main().Render(c, c.Writer)
+	})
+
+	r.POST("/get-document-data", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "File upload (error): %v", err)
+			return
+		}
+
+		pages.Response(file.Filename).Render(c, c.Writer)
 	})
 
 	r.Run(":4000")
