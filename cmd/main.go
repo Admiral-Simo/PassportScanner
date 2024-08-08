1ae00b95f883cb556cc22a31bebe8321ccef276d
Admiral-Simo <admiralsimo16@gmail.com>
2024-08-08 16:44:43 +0100

    fix: clean up the public directory from time to time

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,11 @@ import (
 	"PassportScanner/handlers"
 	"PassportScanner/scannersdk"
 	"PassportScanner/views/pages"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,5 +31,47 @@ func main() {
 	r.GET("/contact", handlers.ContactPageHandler)
 	r.POST("/contact/message/new", handlers.PostDataContactPageHandler(contactStore))
 
+	go cleanUpPublicFile()
+
 	r.Run(":4000")
+}
+
+func cleanUpPublicFile() {
+	for {
+		time.Sleep(time.Hour)
+		deleteUnnecessaryFiles()
+	}
+}
+
+func deleteUnnecessaryFiles() {
+	dir := "./public"
+
+	_ = filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.Name() == "favicon.ico" {
+			return nil
+		}
+
+		if !info.IsDir() {
+			fileInfo, err := os.Stat(path)
+			if err != nil {
+				return err
+			}
+
+			now := time.Now()
+
+			if now.Sub(fileInfo.ModTime()) > 5*time.Minute {
+				err := os.Remove(path)
+				if err != nil {
+					fmt.Printf("Failed to delete file %s: %v\n", path, err)
+				} else {
+					fmt.Printf("Deleted file %s\n", path)
+				}
+			}
+		}
+		return nil
+	})
 }
